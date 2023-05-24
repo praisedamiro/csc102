7d72852d48887ae92eb5d91ad30310688ec6d256
praisedamiro <134234556+praisedamiro@users.noreply.github.com>
2023-05-25 00:44:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 username = input("what is your name: ")
 password = input("Enter your password: ")
 confirm_password = input("please confirm your password: ")
+is_running = True
 
-if password == confirm_password:
-    print('Password validated')
-else:
-    while password != confirm_password:
+while is_running:
+    if password == confirm_password:
+        print('Password validated')
+        is_running = False
+    else:
         print('Passowrd is invalid')
         confirm_password = input("please confirm your password: ")
+        if password == confirm_password:
+            print('Password validated')
+            is_running = False
